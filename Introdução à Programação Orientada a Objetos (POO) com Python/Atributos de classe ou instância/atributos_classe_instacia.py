ee2398b1bc7b996bb91b0fa6a0422ab75e69b201
Rafaelgeomo <jeronymo.rafael@gmail.com>
2024-06-28 15:31:59 -0300

    atributos de classe e desafio

---
+++ Introdução à Programação Orientada a Objetos (POO) com Python/Atributos de classe ou instância/atributos_classe_instacia.py
@@ -0,0 +1,19 @@
+class Estudante:
+   escola = "DIO"
+
+   def __init__(self, nome, numero):
+      self.nome = nome
+      self.numero = numero
+
+   def __str__(self) -> str:
+      return f" {self.nome} - {self.numero} - {self.escola}"
+   
+def mostrar_valores(*objs):
+   for obj in objs:
+      print(obj)
+   
+aluno_1 = Estudante("Guilherme", 15678)
+aluno_2 = Estudante("Giovana", 98676)
+mostrar_valores(aluno_1, aluno_2)
+aluno_1.numero = 158
+mostrar_valores(aluno_1, aluno_2)
